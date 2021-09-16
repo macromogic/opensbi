432dd22314dc533b6d9f1f18b6c40639ee618d04
MstMoonshine <11710919@mail.sustech.edu.cn>
2021-09-16 21:51:54 +0800

    refactor: remove invalid 0xdeadbeef debug magic number

--- include/sbi/sbi_ecall_interface.h
+++ include/sbi/sbi_ecall_interface.h
@@ -29,7 +29,6 @@
 #define SBI_EXT_HSM				0x48534D
 #define SBI_EXT_SRST				0x53525354
 #define SBI_EXT_EBI				0x19260817
-#define SBI_EXT_DEBUG           		0xdeadbeef
 
 /* SBI function IDs for BASE extension*/
 #define SBI_EXT_BASE_GET_SPEC_VERSION		0x0
